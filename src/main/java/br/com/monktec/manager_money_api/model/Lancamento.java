91494bcb129032fd316a38744eed58059999a895
osvaldsoza <osvald.soza@gmail.com>
2018-12-27 19:03:08 -0200

    Cria estrutura do lançamento

---
+++ src/main/java/br/com/monktec/manager_money_api/model/Lancamento.java
@@ -0,0 +1,122 @@
+package br.com.monktec.manager_money_api.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table( name = "lancamento")
+public class Lancamento {
+
+    private Long codigo;
+    private String descricao;
+    private Date dataVencimento;
+    private Date dataPagamento;
+    private BigDecimal valor;
+    private String observacao;
+    private TipoLancamento tipo;
+    private Categoria categoria;
+    private Pessoa pessoa;
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    @NotNull
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Column(name = "data_vencimento")
+    @NotNull
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    @Column(name = "data_pagamento")
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    @NotNull
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    public TipoLancamento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoLancamento tipo) {
+        this.tipo = tipo;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_categoria")
+    @NotNull
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_pessoa")
+    @NotNull
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Lancamento)) return false;
+        Lancamento that = (Lancamento) o;
+        return Objects.equals(codigo, that.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
---
+++ src/main/java/br/com/monktec/manager_money_api/model/TipoLancamento.java
@@ -0,0 +1,6 @@
+package br.com.monktec.manager_money_api.model;
+
+public enum TipoLancamento {
+    RECEITA,
+    DESPESA
+}
