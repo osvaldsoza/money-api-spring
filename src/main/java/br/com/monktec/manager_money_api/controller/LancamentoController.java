c2fb8594345828f11b4ebe04fa50cc74b2b5645e
osvaldsoza <osvald.soza@gmail.com>
2018-12-27 21:52:43 -0200

    Atualiza o controller e o service lancamento

--- src/main/java/br/com/monktec/manager_money_api/controller/LancamentoController.java
+++ src/main/java/br/com/monktec/manager_money_api/controller/LancamentoController.java
@@ -1,4 +1,46 @@
 package br.com.monktec.manager_money_api.controller;
 
+import br.com.monktec.manager_money_api.event.MoneyApiEvent;
+import br.com.monktec.manager_money_api.model.Lancamento;
+import br.com.monktec.manager_money_api.service.LancamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/lancamentos")
 public class LancamentoController {
+
+    @Autowired
+    private LancamentoService lancamentoService;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @GetMapping
+    public List<Lancamento> getLancamentos() {
+        return lancamentoService.listarLancamentos();
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Lancamento> getLancamentoPeloCodigo(@PathVariable Long codigo) {
+        Lancamento lancamentoSalvo = lancamentoService.buscarLancamentoPeloCodigo(codigo);
+
+        return lancamentoSalvo != null ? ResponseEntity.ok(lancamentoSalvo) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> postLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+        Lancamento lancamentoSalvo = lancamentoService.salvarLancamento(lancamento);
+
+        publisher.publishEvent(new MoneyApiEvent(this, response, lancamentoSalvo.getCodigo()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+    }
 }
--- src/main/java/br/com/monktec/manager_money_api/service/LancamentoService.java
+++ src/main/java/br/com/monktec/manager_money_api/service/LancamentoService.java
@@ -1,4 +1,27 @@
 package br.com.monktec.manager_money_api.service;
 
+import br.com.monktec.manager_money_api.model.Lancamento;
+import br.com.monktec.manager_money_api.repository.LancamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LancamentoService {
+
+    @Autowired
+    private LancamentoRepository lancamentoRepository;
+
+    public List<Lancamento> listarLancamentos(){
+        return lancamentoRepository.findAll();
+    }
+
+    public Lancamento buscarLancamentoPeloCodigo(Long codigo){
+        return lancamentoRepository.findOne(codigo);
+    }
+
+    public Lancamento salvarLancamento(Lancamento lancamento){
+        return lancamentoRepository.save(lancamento);
+    }
 }
